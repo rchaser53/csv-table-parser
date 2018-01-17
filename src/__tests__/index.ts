5e5eede721791121f7619af3937d9fe5302feef1
rchaser53 <tayoshizawa29@gmail.com>
2018-01-17 23:02:03 +0900

    add some normal tests

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,17 +1,37 @@
-import {
-    convertCsvToObject
-} from '..'
-  
-  
-  describe('convertCsvToObject', () => {
-    test('convert csv to object', async() => {
-        const input = `a,b,c
+import { convertCsvToObject } from '..'
+
+describe('convertCsvToObject', () => {
+	test('convert csv to object', async () => {
+		const input = `a,b,c
 1,2,3`
-   
-      expect(convertCsvToObject(input)).toEqual([
-          {
-              a: '1', b: '2', c: '3'
-          }
-      ])
-    });
-  });
+
+		expect(convertCsvToObject(input)).toEqual([
+			{
+				a: '1',
+				b: '2',
+				c: '3'
+			}
+		])
+	})
+
+	test('return empty array when first parameter is ""', async () => {
+		const input = ``
+
+		expect(convertCsvToObject(input)).toEqual([])
+	})
+})
+
+describe('convertCsvToObject using tab for separator', () => {
+	test('convert tsv to object', async () => {
+		const input = `a	b	c
+1	2	3`
+
+		expect(convertCsvToObject(input, '\t')).toEqual([
+			{
+				a: '1',
+				b: '2',
+				c: '3'
+			}
+		])
+	})
+})
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ export interface AnyObject {
 	[key: string]: any
 }
 
-export const convertCsvToObject = (tsvString: string, Separate = ','): AnyObject => {
+export const convertCsvToObject = (tsvString: string, separator = ','): AnyObject => {
 	const rows = tsvString.split('\n')
-	const keys = rows.length > 0 ? (rows.shift() || '').split(Separate) : []
+	const keys = rows.length > 0 ? (rows.shift() || '').split(separator) : []
 
 	return rows.reduce<AnyObject[]>((stack, next) => {
-		const values = next.split(Separate)
+		const values = next.split(separator)
 		const rowDataObject = values.reduce<AnyObject>((obj, elem, index) => {
 			obj[keys[index]] = elem
 			return obj
